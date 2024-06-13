f85d08b4ddfe7c66c9d9fd5ea71846bd64b5bcad
= <=>
2024-06-13 18:19:44 +0530

    Navlink active working

--- js/data/navItem.js
+++ js/data/navItem.js
@@ -1,5 +1,18 @@
-export const navItems = {
+export const navItems = [
+
     {
-        ""
+        "name":"Products",
+        "link":"/products",
+        "id":"nav-products"
+    },
+    {
+        "name":"About us",
+        "link":"/about",
+        "id":"nav-about"
+    },
+    {
+        "name":"Contact Us",
+        "link":"/contact",
+        "id":"nav-contact"
     }
-}
+]
--- js/nav.js
+++ js/nav.js
@@ -4,9 +4,48 @@ let path_str = document.location.pathname
 
 
 const paths = path_str.split("/")
+console.log(paths[1].split(".")[0])
 
-if(paths[0] == "about"){
-    document.getElementBy
+const navItem_about = document.getElementsByClassName("nav-active-about")
+const navItem_products = document.getElementsByClassName("nav-active-products")
+const navItem_contact = document.getElementsByClassName("nav-active-contact")
+
+
+
+function toggleClass(elements, className, action) {
+    for (let i = 0; i < elements.length; i++) {
+        if (action === "add") {
+            elements[i].classList.add(className);
+        } else if (action === "remove") {
+            elements[i].classList.remove(className);
+        }
+    }
+}
+
+if(paths[1].split(".")[0] === "about"){
+
+    toggleClass(navItem_about, "hidden","remove")
+    toggleClass(navItem_products, "hidden","add")
+    toggleClass(navItem_contact, "hidden","add")
+
+
+
+
+}
+if(paths[1].split(".")[0] === "contact"){
+
+    toggleClass(navItem_about, "hidden","add")
+    toggleClass(navItem_products, "hidden","add")
+    toggleClass(navItem_contact, "hidden","remove")
+
+}
+if(paths[1].split(".")[0] === "products"){
+
+
+    toggleClass(navItem_about, "hidden","add")
+    toggleClass(navItem_products, "hidden","remove")
+    toggleClass(navItem_contact, "hidden","add")
+
 }
 
 
